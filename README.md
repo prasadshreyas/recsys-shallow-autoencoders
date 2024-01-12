b8913df00ce990d84cd38d6c620a1168d5468b09
Shreyas Prasad <iamshreyasprasad@gmail.com>
2024-01-12 01:44:29 -0500

    final

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# Spotify-RecSys-WebApp
- 
+# Song Recommender for Playlists Using EASE
+
+## Overview
+This system recommends tracks for Spotify playlists using Embarrassingly Shallow Autoencoders for Sparse Data (EASE), trained on the Million Playlist Dataset (MPD).
+
+## Dataset
+MPD includes 1,000,000 Spotify user-created playlists with titles, track lists, editing information, and other details.
+
+<img src="res/demo.gif" width="750">
+
+## Installation
+Required libraries:
+- json
+- csv
+- pandas
+- numpy
+- os
+- sys
+- logging
+- torch
+- streamlit
+
+## Data Preprocessing
+The dataset is split into 1,000 slice files (`mpd.slice.0-999.json`). The script converts these files into Pandas DataFrames.
+
+## Feature Engineering
+Tracks in playlists are converted into a DataFrame. A utility matrix is built for the recommender system.
+
+## Model Architecture
+A Torch-based EASE model. It builds user and item lookup tables, creates a sparse data representation, and predicts track recommendations.
+
+## Usage
+1. Load data using `loadData`.
+2. Select a playlist in Streamlit.
+3. View recommendations.
+
+## Example
+```python
+df = loadData('data/tracks_playlist_500.pkl')
+res = loadData('data/res.pkl')
+main()
+```
+
+## References
+- [EASE Paper](https://arxiv.org/pdf/1905.03375.pdf)
+- [Variational Autoencoders for Collaborative Filtering](https://arxiv.org/abs/1802.05814)
+- [Spotify MPD](https://www.aicrowd.com/challenges/spotify-million-playlist-dataset-challenge)
+
+## Author
+[Your Name]
+
+---
+
+This README provides essential information about the project, including its purpose, dataset, installation instructions, features, usage, and references.
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from email import header
-from platform import architecture
 from re import A
 import streamlit as st
 import random as rd
@@ -43,39 +41,52 @@ def getPlaylistNumber(df, p_name):
     '''
     return df[df['playlist_name']==p_name]['pid'].tolist()[0]
 
+def main():
+    st.title("Song Recommender for Playlists")
+    st.markdown(
+        """
+        The model has been trained on the Million Playlist Dataset (MPD) which contains 1,000,000 playlists from Spotify.
+        We use EASE (Embarrassingly Shallow Autoencoders) developed by Harald Steck to recommend songs to a playlist. Link to the paper [here.](https://dl.acm.org/doi/pdf/10.1145/3308558.3313710?casa_token=1cGSrp9S7HAAAAAA:dBC4UCzkQyILJlH46hZVWqGbDeEMpTm44JnllDY5XC3Kis_D6H4KVA1DCQ38OzKTmwC7O04btPpxQOE)
+        """
+    )
+
+
+    # Playlist selection column
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.markdown(
+            """
+            #### Select a playlist from the dropdown menu to see the recommended songs for that playlist.
+            """
+        )
+        pid_list = randomNumGenerator(n)
+        playlist_options = listPlaylistNames(df, pid_list)
+        option = st.selectbox('List of Randomly Generated Playlists', playlist_options, key="playlist_selector")
+        pid = getPlaylistNumber(df, option)
+
+        # Displaying songs in an interactive format
+        with st.expander("See Songs in the Playlist"):
+            for song in listTracksNamesAndArtistsInPlaylist(df, pid):
+                st.text(song)
+
+    # Recommendation column
+    with col2:
+
+        st.markdown(
+            """
+            #### See the recommended songs for the playlist selected on the left.
+            """
+        )
+        recommended_songs = listTracksNamesAndArtistsInPlaylist(res, pid)
+        for song in recommended_songs:
+            st.text(song)
+
+
+
 if __name__ == '__main__':
     df = loadData('data/tracks_playlist_500.pkl')
     res = loadData('data/res.pkl')
-    pid_list = randomNumGenerator(n)
     markdown1 ='''
-
-    A collaborative filtering technique to recommend tracks to a playlist based on similair playlist/s. \n 
-    Visit this [link](https://shreyasprasad.com/paper/2022/06/01/Spotify-Recommendations-Using-Embarrassingly-Shallow-Autoencoders-for-Sparse-Data.html) for more info
-    '''
-
-
-# -----------------------    Interface              ---------------------------------------------------------
-
-header = st.container()
-c1 = st.container()
-c2 = st.container()
-c3 = st.container()
-
-with header:
-    st.title("Spotify Recommender for Playlists")
-    st.markdown(markdown1)
-
-with c1:
-    st.header("Select Playlist")
-    option = st.selectbox( 'List of Randomly generated playlists', listPlaylistNames(df, pid_list))
-    pid = getPlaylistNumber(df, option)
-
-
-with c2:
-    st.header("Songs in the Playlist: " + option)
-    st.write(listTracksNamesAndArtistsInPlaylist(df, pid))
-
-
-with c3:
-    st.header("Recommendation for the Playlist: " + option)
-    st.write(listTracksNamesAndArtistsInPlaylist(res, pid))
+    A collaborative filtering technique to recommend tracks to a playlist based on similair playlist/s.'''
+    main()
